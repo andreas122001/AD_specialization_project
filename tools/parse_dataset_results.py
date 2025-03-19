3120651ecf4a0b347c486ef3cae60e8bbc387226
Andreas Winje <andrebw@stud.ntnu.no>
2025-03-19 18:29:05 +0100

    created script for aggregating dataset results, and added some notes

---
+++ tools/parse_dataset_results.py
@@ -0,0 +1,65 @@
+import glob
+import gzip
+import numpy as np
+from tqdm import tqdm
+import ujson
+
+root = "/cluster/work/andrebw/repos/temporal_garage/results/data/garage_v2_2025_03_15/results"
+
+result_files = glob.glob(f"{root}/**/*_result.json", recursive=True)
+
+# Scenarios
+results = {}
+for result_path in tqdm(result_files):
+    scenario = result_path.split("/")[-2]
+    if scenario not in results.keys():
+        results[scenario] = []
+    with open(result_path, "rt", encoding="utf-8") as f:
+        results_route = ujson.load(f)
+        if "scores_mean" not in results_route["_checkpoint"]["global_record"]:
+            continue
+        results[scenario].append(results_route["_checkpoint"]["global_record"]["scores_mean"])
+
+# Post-process 
+for scenario, scenario_res in results.items():
+    score_route = [res["score_route"] for res in scenario_res]
+    score_penalty = [res["score_penalty"] for res in scenario_res]
+    score_composed = [res["score_composed"] for res in scenario_res]
+    # Just overwrite with the averages
+    results[scenario] = {
+        "scores_mean": {
+            "score_route": np.mean(score_route),
+            "score_penalty": np.mean(score_penalty),
+            "score_composed": np.mean(score_composed)
+        },
+        "scores_std": {
+            "score_route": np.std(score_route),
+            "score_penalty": np.std(score_penalty),
+            "score_composed": np.std(score_composed)
+        }
+    }
+
+scores_route = [entry["scores_mean"]["score_route"] for entry in results.values()]
+score_penalty = [entry["scores_mean"]["score_penalty"] for entry in results.values()]
+score_composed = [entry["scores_mean"]["score_composed"] for entry in results.values()]
+
+results['Total'] = {
+    "scores_mean": {
+        "score_route": np.mean(scores_route),
+        "score_penalty": np.mean(score_penalty),
+        "score_composed": np.mean(score_composed)
+    },
+    # Notice, the std here is across scenarios, not routes in total
+    "scores_std": {
+        "score_route": np.std(scores_route),
+        "score_penalty": np.std(score_penalty),
+        "score_composed": np.std(score_composed)
+    },
+}
+
+
+from pathlib import Path
+save_path = "/cluster/work/andrebw/repos/temporal_garage/dataset_results.json"
+print(Path(save_path))
+with open(save_path, "w") as f:
+    ujson.dump(results, f, indent=4)
