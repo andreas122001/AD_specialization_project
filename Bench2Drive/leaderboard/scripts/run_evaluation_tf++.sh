5eefe85e2433d0184fa745ec834e0dcffcae3b4d
Andreas Winje <andrebw@stud.ntnu.no>
2025-05-05 10:02:17 +0200

    fix bench2drive script

--- Bench2Drive/leaderboard/scripts/run_evaluation_tf++.sh
+++ Bench2Drive/leaderboard/scripts/run_evaluation_tf++.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
-#SBATCH --job-name=b2d_009
+#SBATCH --job-name=b2d/static-LB5s1-L2
+#SBATCH --account=share-ie-idi
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --time=2-00:00
-#SBATCH --gres=gpu:8
-#SBATCH --cpus-per-task=64
-#SBATCH --output=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/results/logs/b2d_009_%a_%A.out  # File to which STDOUT will be written
-#SBATCH --error=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/results/logs/b2d_009_%a_%A.err   # File to which STDERR will be written
-#SBATCH --partition=2080-galvani
+#SBATCH --time=0-16:00:00
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32gb
+#SBATCH --output=/cluster/work/andrebw/repos/temporal_garage/evaluation/bench2drive/logs/b2d_009_%a_%A.out  # File to which STDOUT will be written
+#SBATCH --error=/cluster/work/andrebw/repos/temporal_garage/evaluation/bench2drive/logs/b2d_009_%a_%A.out   # File to which STDERR will be written
+#SBATCH --partition=GPUQ
 
-export CARLA_ROOT=/mnt/lustre/work/geiger/bjaeger25/CARLA_0_9_15
-export WORK_DIR=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/Bench2Drive
+export CARLA_ROOT=/cluster/work/andrebw/repos/temporal_garage/carla
+export WORK_DIR=/cluster/work/andrebw/repos/temporal_garage/Bench2Drive
 export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
 export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
-export PYTHONPATH=$PYTHONPATH:/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/team_code
+export PYTHONPATH=$PYTHONPATH:/cluster/work/andrebw/repos/temporal_garage/team_code
 export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
+
+export MODEL="static-LB5s1-L2"
+export CKPT=$MODEL"_model_0030"
+
+export NGPUS=$(echo $SLURM_JOB_GPUS | grep -oP [0-9]+ | wc -l)
+echo SLURM_JOB_GPUS: $SLURM_JOB_GPUS
+echo Num GPUS: $NGPUS
+gpu_list=(${SLURM_JOB_GPUS//,/ })
+echo $gpu_list
+echo $(echo $SLURM_JOB_GPUS | sed -e "s/,/ /g")
 
 #!/bin/bash
 BASE_PORT=30000
 BASE_TM_PORT=50000
 IS_BENCH2DRIVE=True
 BASE_ROUTES=${WORK_DIR}/leaderboard/data/bench2drive220
-TEAM_AGENT=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/team_code/sensor_agent.py
-# Must set YOUR_CKPT_PATH
-TEAM_CONFIG=/mnt/lustre/work/geiger/bjaeger25/garage_2_cleanup/team_code/checkpoints/tfpp_009_ensemble_0_1_2
+TEAM_AGENT=/cluster/work/andrebw/repos/temporal_garage/team_code/sensor_agent.py
+TEAM_CONFIG=/cluster/work/andrebw/repos/temporal_garage/team_code/checkpoints/$CKPT
 BASE_CHECKPOINT_ENDPOINT=eval_bench2drive220
 PLANNER_TYPE=traj
-ALGO=tfpp
+ALGO=$MODEL
 SAVE_PATH=${WORK_DIR}/leaderboard/data/eval_bench2drive220_${ALGO}_${PLANNER_TYPE}
 
 if [ ! -d "${ALGO}_b2d_${PLANNER_TYPE}" ]; then
@@ -40,8 +51,8 @@ fi
 if [ ! -f "${BASE_ROUTES}_${ALGO}_${PLANNER_TYPE}_split_done.flag" ]; then
     echo -e "****************************\033[33m Attention \033[0m ****************************"
     echo -e "\033[33m Running split_xml.py \033[0m"
-    TASK_NUM=8 # 8*H100, 1 task per gpu
-    python ${WORK_DIR}/tools/split_xml.py $BASE_ROUTES $TASK_NUM $ALGO $PLANNER_TYPE
+    TASK_NUM=$NGPUS # x*GPU, 1 task per gpu
+    python -u ${WORK_DIR}/tools/split_xml.py $BASE_ROUTES $TASK_NUM $ALGO $PLANNER_TYPE
     touch "${BASE_ROUTES}_${ALGO}_${PLANNER_TYPE}_split_done.flag"
     echo -e "\033[32m Splitting complete. Flag file created. \033[0m"
 else
@@ -50,10 +61,10 @@ fi
 
 echo -e "**************\033[36m Please Manually adjust GPU or TASK_ID \033[0m **************"
 # Example, 8*H100, 1 task per gpu
-GPU_RANK_LIST=(0 1 2 3 4 5 6 7)
-TASK_LIST=(0 1 2 3 4 5 6 7)
-echo -e "\033[32m GPU_RANK_LIST: $GPU_RANK_LIST \033[0m"
-echo -e "\033[32m TASK_LIST: $TASK_LIST \033[0m"
+IFS=',' read -ra GPU_RANK_LIST <<< "$SLURM_JOB_GPUS"
+TASK_LIST=( $(seq 0 $(($NGPUS-1))) )
+echo -e "\033[32m GPU_RANK_LIST: ${GPU_RANK_LIST[*]} \033[0m"
+echo -e "\033[32m TASK_LIST: ${TASK_LIST[*]} \033[0m"
 echo -e "***********************************************************************************"
 
 length=${#GPU_RANK_LIST[@]}
@@ -61,8 +72,8 @@ for ((i=0; i<$length; i++ )); do
       PORT=$((BASE_PORT + i * 150))
       TM_PORT=$((BASE_TM_PORT + i * 150))
       ROUTES="${BASE_ROUTES}_${TASK_LIST[$i]}_${ALGO}_${PLANNER_TYPE}.xml"
-      CHECKPOINT_ENDPOINT="${WORK_DIR}/${ALGO}_b2d_${PLANNER_TYPE}/${BASE_CHECKPOINT_ENDPOINT}_${TASK_LIST[$i]}.json"
-      mkdir -p "${WORK_DIR}/${ALGO}_b2d_${PLANNER_TYPE}"
+      CHECKPOINT_ENDPOINT="${WORK_DIR}/../evaluation/bench2drive/${ALGO}_b2d_${PLANNER_TYPE}/${BASE_CHECKPOINT_ENDPOINT}_${TASK_LIST[$i]}.json"
+      mkdir -p "${WORK_DIR}/../evaluation/bench2drive/${ALGO}_b2d_${PLANNER_TYPE}"
       GPU_RANK=${GPU_RANK_LIST[$i]}
       echo -e "\033[32m ALGO: $ALGO \033[0m"
       echo -e "\033[32m PLANNER_TYPE: $PLANNER_TYPE \033[0m"
@@ -77,3 +88,4 @@ for ((i=0; i<$length; i++ )); do
       sleep 5
 done
 wait
+echo All completed!
